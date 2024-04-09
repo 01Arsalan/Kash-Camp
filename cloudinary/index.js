baff97090440a1d23a3f443b718b719fabe34a8f
ArsalanWani <arsalanwane@gmail.com>
2024-04-09 18:51:51 +0530

    success

---
+++ cloudinary/index.js
@@ -0,0 +1,23 @@
+//refer to docs for elucidation
+
+const cloudinary = require("cloudinary").v2
+const { CloudinaryStorage } = require("multer-storage-cloudinary")
+
+cloudinary.config({
+    cloud_name: process.env.CLOUDINARY_CLOUD_NAME,
+    api_key: process.env.CLOUDINARY_KEY,
+    api_secret: process.env.CLOUDINARY_SECRET
+})
+
+const storage = new CloudinaryStorage({
+    cloudinary,
+    params: {
+        folder: "yelp-camp",
+        allowedFormats: ['jpeg,jpg,png']
+    }
+})
+
+module.exports = {
+    cloudinary,
+    storage
+}
