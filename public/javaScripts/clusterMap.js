9f8615f1926f000b0723d752fe5dccf17ee06083
01Arsalan <109332699+01Arsalan@users.noreply.github.com>
2024-04-10 15:25:27 +0530

    Update clusterMap.js

--- public/javaScripts/clusterMap.js
+++ public/javaScripts/clusterMap.js
@@ -4,7 +4,7 @@ const map = new mapboxgl.Map({
     container: 'cluster-map',
     // Choose from Mapbox's core styles, or make your own style with Mapbox Studio
     style: 'mapbox://styles/mapbox/dark-v11',
-    center: [-103.5917, 40.6699],
+    center: [74.797371, 34.083656],
     zoom: 3
 });
 
